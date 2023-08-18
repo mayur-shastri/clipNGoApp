d3f31fce71967d4632a6a98e599e88588e9c74e9
sagnikMajumder89 <sagnikm183@gmail.com>
2023-08-18 19:15:46 +0530

    profile_image locally stored

---
+++ lib/providers/profile_image_provider.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final profileImageProvider = StateProvider((ref) {
+  return '';
+});
--- lib/widgets/user image picker/image_picker.dart
+++ lib/widgets/user image picker/image_picker.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:salon_app/widgets/user%20image%20picker/take_image_button.dart';
 import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class ProfieImagePicker extends StatefulWidget {
   const ProfieImagePicker({super.key});
@@ -13,11 +15,49 @@ class ProfieImagePicker extends StatefulWidget {
 class _ProfieImagePickerState extends State<ProfieImagePicker> {
   late ImagePicker picker;
   XFile? _userImage;
+  File? savedImage;
+  bool loadingImage = true;
+
+  Future<void> saveImageToDevice(XFile image) async {
+    var status = await Permission.storage.status;
+    if (!status.isGranted) {
+      await Permission.storage.request();
+    }
+    // Get the directory for storing files
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    String savedImagePath = '${appDocDir.path}/saved_image.png';
+    //checking if file exists
+    File existingFile = File(savedImagePath);
+    if (await existingFile.exists()) {
+      await existingFile.delete();
+    }
+    // Read the image bytes from XFile
+    List<int> imageBytes = await image.readAsBytes();
+
+    // Write the image bytes to a new file
+    savedImage = File(savedImagePath);
+    await savedImage!.writeAsBytes(imageBytes);
+    checkForSavedImage();
+    print('Image saved to: $savedImagePath');
+  }
+
+  Future<void> checkForSavedImage() async {
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    String savedImagePath = '${appDocDir.path}/saved_image.png';
+    File imageFile = File(savedImagePath);
+    if (await imageFile.exists()) {
+      setState(() {
+        savedImage = imageFile;
+        loadingImage = false;
+      });
+    }
+  }
 
   @override
   void initState() {
     super.initState();
     picker = ImagePicker();
+    checkForSavedImage();
   }
 
   void showModalSheet() {
@@ -52,15 +92,20 @@ class _ProfieImagePickerState extends State<ProfieImagePicker> {
     if (pickedFile != null) {
       setState(() {
         _userImage = pickedFile;
+        saveImageToDevice(pickedFile);
       });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    final content = _userImage != null
-        ? FileImage(File(_userImage!.path))
-        : const AssetImage('asset/images/profile_image_add.png');
+    final content = loadingImage
+        ? const AssetImage('asset/images/loadingImageGIF.gif')
+        : (_userImage != null
+            ? FileImage(File(_userImage!.path))
+            : savedImage != null
+                ? FileImage(savedImage!)
+                : const AssetImage('asset/images/profile_image_add.png'));
     return Center(
       child: Card(
         shape: const CircleBorder(),
