00e879ddb654348a30c1072edb250744dc8baa85
Mau_2993 <shastrimayur2993@gmail.com>
2023-10-05 18:10:18 +0530

    search engine almost done

--- lib/screen/screen_list.dart
+++ lib/screen/screen_list.dart
@@ -1,16 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:salon_app/widgets/dashboard%20items/search_bar.dart';
+import 'package:salon_app/widgets/search_results_list.dart';
 
-class ScreenList extends StatelessWidget {
+class ScreenList extends StatefulWidget {
   const ScreenList({super.key});
+
+  @override
+  State<ScreenList> createState() => _ScreenListState();
+}
+
+class _ScreenListState extends State<ScreenList> {
+  List <Map<String, dynamic>> _searchResults = [];
+
+  void _onSearchResultsUpdated() {
+    setState(() {
+      _searchResults = searchResults;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Search your nearest salons'),
+          title: const Text('Search your nearest salons'),
         ),
         body: Column(
-          children: [],
+          children: [
+            Center(
+              child: MySearchBar(
+                  isMainPage: false,
+                  onSearchResultsUpdated: _onSearchResultsUpdated),
+            ),
+            Expanded(
+              child: SearchResultsList(latestSearchResults: _searchResults),
+            ),
+          ],
         ));
   }
 }
